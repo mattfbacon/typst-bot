151d8df44f94b3ff9390143aa268a1253a149e00
Matt Fellenz <matt+github@felle.nz>
2025-08-25 00:20:18 +0200

    Clean up token-from-file logic

--- crates/bot/src/bot.rs
+++ crates/bot/src/bot.rs
@@ -754,6 +754,14 @@ async fn handle_error(
 }
 
 pub async fn run() {
+	let token = match (std::env::var_os("DISCORD_TOKEN"), std::env::var_os("DISCORD_TOKEN_FILE")) {
+		(Some(token), None) => token.into_string().expect("`DISCORD_TOKEN` not UTF-8"),
+		(None, Some(path)) => std::fs::read_to_string(path).expect("reading from `DISCORD_TOKEN_FILE`"),
+		(Some(_token), Some(_path)) => panic!("both `DISCORD_TOKEN` and `DISCORD_TOKEN_FILE` provided.\nThis is ambiguous and insecure. Please only use one or the other."),
+		(None, None) => panic!("need `DISCORD_TOKEN` or `DISCORD_TOKEN_FILE` env var"),
+	};
+	let token = token.trim();
+
 	let database = Connection::open_with_flags(
 		std::env::var_os("DB_PATH").expect("need `DB_PATH` env var"),
 		OpenFlags::SQLITE_OPEN_READ_WRITE | OpenFlags::SQLITE_OPEN_CREATE,
@@ -765,16 +773,6 @@ pub async fn run() {
 	let pool = Worker::spawn().await.unwrap();
 
 	let edit_tracker_time = std::time::Duration::from_secs(3600);
-
-	let token = std::env::var("DISCORD_TOKEN")
-		.or_else(|_| {
-			// check if DISCORD_TOKEN_FILE is set
-			let file_path = std::env::var("DISCORD_TOKEN_FILE")
-				.map_err(|_| std::io::Error::new(std::io::ErrorKind::NotFound, "DISCORD_TOKEN_FILE not set"))?;
-				std::fs::read_to_string(file_path).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
-		})
-		.map(|s| s.trim().to_string())
-		.expect("need `DISCORD_TOKEN` env var or DISCORD_TOKEN_FILE pointing to a secret");
 
 	let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
 	let framework = poise::Framework::builder()
